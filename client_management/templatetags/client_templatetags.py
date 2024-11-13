6a19cdad95cdb993fa6eccbe83835b6d245087f1
ramshidmobiz <ramshid@mobiz.com>
2024-11-14 00:27:13 +0530

    outstanding

--- client_management/templatetags/client_templatetags.py
+++ client_management/templatetags/client_templatetags.py
@@ -59,13 +59,15 @@ def route_wise_customer_bottle_count(customer_pk):
         
 @register.simple_tag
 def get_outstanding_amount(customer_id,date):
+    if not customer_id:  # Ensure customer_id is not empty
+        return 0 
     outstanding_amounts = OutstandingAmount.objects.filter(customer_outstanding__customer__pk=customer_id,customer_outstanding__created_date__date__lte=date).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
     collection_amount = CollectionPayment.objects.filter(customer__pk=customer_id,created_date__date__lte=date).aggregate(total_amount_received=Sum('amount_received'))['total_amount_received'] or 0
     
-    if outstanding_amounts > collection_amount:
-        return outstanding_amounts - collection_amount
-    else:
-        return collection_amount - outstanding_amounts
+    return outstanding_amounts - collection_amount
+    # if outstanding_amounts > collection_amount:
+    # else:
+    #     return collection_amount - outstanding_amounts
 
 @register.simple_tag
 def get_outstanding_bottles(customer_id, date):
